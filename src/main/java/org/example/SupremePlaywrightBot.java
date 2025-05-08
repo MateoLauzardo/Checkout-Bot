5686119129071caf01af2d042e2cc392b7ba8ddd
MateoLauzardo <mateo.lauzardo@hotmail.com>
2025-05-08 15:39:50 -0400

    line 9 comment

--- src/main/java/org/example/SupremePlaywrightBot.java
+++ src/main/java/org/example/SupremePlaywrightBot.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 
 public class SupremePlaywrightBot {
     public static void main(String[] args) throws IOException {
-
+            // Open browser
             Playwright playwright = Playwright.create();
             Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
             Page page = browser.newPage();
